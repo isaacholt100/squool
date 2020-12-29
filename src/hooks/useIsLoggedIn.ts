18b256d7ac7afe266fab168ea48b4450271c2a8d
isaac-holt <isaac_holt@icloud.com>
2020-12-29 20:13:17 +0000

    Better offline detection

--- src/hooks/useIsLoggedIn.ts
+++ src/hooks/useIsLoggedIn.ts
@@ -6,13 +6,20 @@ export default function useIsLoggedIn() {
     const [, setOnline] = useIsOnline();
     const l = Boolean(Cookies.get("refreshToken") && Cookies.get("accessToken"));
     const { data, error } = useSWR("/api/login", async url => {
+        let offline = true;
         try {
-            const res = await fetch(url);
+            const res = await fetch(url, {
+                headers: {
+                    "cache-control": "no-cache",
+                    "pragma": "no-cache",
+                }
+            });
+            offline = false;
             const json = res.json();
             process.env.NODE_ENV === "production" && setOnline(true);
             return json;
         } catch (err) {
-            process.env.NODE_ENV === "production" && setOnline(false);
+            process.env.NODE_ENV === "production" && offline && setOnline(false);
             throw err;
         }
     }, {
