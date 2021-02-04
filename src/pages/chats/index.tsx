9e0e407c1a4246207130fb2a69f7c64f091eef50
isaac-holt <isaac_holt@icloud.com>
2021-02-04 20:19:31 +0000

    No chat message

--- src/pages/chats/index.tsx
+++ src/pages/chats/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Hidden, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, Paper, TextField, Tooltip, useMediaQuery } from "@material-ui/core";
+import { Box, Button, Hidden, IconButton, List, ListItem, ListItemSecondaryAction, ListItemText, Paper, TextField, Tooltip, Typography, useMediaQuery } from "@material-ui/core";
 import { mdiDotsHorizontalCircle } from "@mdi/js";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -15,7 +15,7 @@ interface IChat {
 interface ISideBarProps {
     list: IChat[];
     chat_id: string;
-    isSmall: boolean;
+    hidden: boolean;
 }
 
 function SideBar(props: ISideBarProps) {
@@ -34,7 +34,7 @@ function SideBar(props: ISideBarProps) {
     return (
         <>
             <div className={styles.sidebar_root}>
-                <Paper className={clsx("p_6 pb_0 flex flex_col", styles.list_container)} style={props.isSmall && props.chat_id ? {display: "none"} : {}}>
+                <Paper className={clsx("p_6 pb_0 flex flex_col", styles.list_container)} style={props.hidden ? {display: "none"} : {}}>
                     <Button color="secondary" fullWidth className="mb_6">
                         New Chat
                     </Button>
@@ -68,10 +68,18 @@ export default function Chats() {
     const
         router = useRouter(),
         chat_id = router.query.id as string,
-        isSmall = useMediaQuery("(max-width:680px)");
+        isSmall = useMediaQuery("(max-width:680px)"),
+        listHidden = isSmall && !chat_id;
     return (
         <div className="flex">
-            <SideBar list={Array(12).fill(undefined)} chat_id={chat_id} isSmall={isSmall} />
+            <SideBar list={Array(12).fill(undefined)} chat_id={chat_id} hidden={listHidden} />
+            <Box className="flex_1" ml={{md: isSmall ? 0 : 1, lg: 2,}}>
+                {!isSmall && !chat_id && (
+                    <div className="flex align_items_center flex_col">
+                        <Typography variant="h6">No chat selected</Typography>
+                    </div>
+                )}
+            </Box>
         </div>
     );
 }
